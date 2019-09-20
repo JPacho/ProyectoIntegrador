bb94525e5641c8a324c9de0f6fd908af540b0b57
Francisco <fcojair.dico@gmail.com>
2019-09-20 12:41:49 -0500

    Se agregaron archivos de compilacion

--- Debug/synergy/ssp/src/driver/r_gpt/r_gpt.d
+++ Debug/synergy/ssp/src/driver/r_gpt/r_gpt.d
@@ -1,155 +1,155 @@
 synergy/ssp/src/driver/r_gpt/r_gpt.o: \
  ../synergy/ssp/src/driver/r_gpt/r_gpt.c \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_gpt.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp/bsp_api.h \
- c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
- c:\dse\proyectointegrador\synergy\ssp\inc\ssp_version.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
- c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp.h \
- c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_init.h \
- c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_leds.h \
- c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_qspi.h \
- c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
- c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_ioport_api.h \
- c:\dse\proyectointegrador\synergy\ssp\inc\ssp_features.h \
- c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_fmi.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_fmi_api.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
- c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_timer_api.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_gpt_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_gpt.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_version.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp.h \
+ c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_init.h \
+ c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_features.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_timer_api.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_gpt_cfg.h \
  ../synergy/ssp/src/driver/r_gpt/hw/hw_gpt_private.h \
  ../synergy/ssp/src/driver/r_gpt/hw/common/hw_gpt_common.h \
  ../synergy/ssp/src/driver/r_gpt/r_gpt_private_api.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_cgc.h \
- C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h \
- C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_cgc_api.h
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_cgc.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h \
+ C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_cgc_api.h
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_gpt.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_gpt.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp/bsp_api.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp/bsp_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\inc\ssp_version.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_version.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
 
-c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp.h:
+c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp.h:
 
-c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_init.h:
+c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_init.h:
 
-c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_leds.h:
+c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_leds.h:
 
-c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_qspi.h:
+c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_qspi.h:
 
-c:\dse\proyectointegrador\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+c:\dse\equipo8\proyectointegrador\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
 
-c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_ioport_api.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_ioport_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\inc\ssp_features.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_features.h:
 
-c:\dse\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\inc\ssp_common_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_fmi.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_fmi.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_fmi_api.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_fmi_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
 
-c:\dse\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
+c:\dse\equipo8\proyectointegrador\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_timer_api.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_timer_api.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_gpt_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_gpt_cfg.h:
 
 ../synergy/ssp/src/driver/r_gpt/hw/hw_gpt_private.h:
 
@@ -157,8 +157,8 @@ C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_gpt_cfg.h:
 
 ../synergy/ssp/src/driver/r_gpt/r_gpt_private_api.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_cgc.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\instances/r_cgc.h:
 
-C:\DSE\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h:
 
-C:\DSE\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_cgc_api.h:
+C:\DSE\EQUIPO8\ProyectoIntegrador\synergy\ssp\inc\driver\api/r_cgc_api.h:
