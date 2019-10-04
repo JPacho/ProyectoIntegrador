fd7c132d0c34eedbbe4da20f18cf7c56d0052f19
Francisco <fcojair.dico@gmail.com>
2019-10-04 17:56:00 -0500

    Limpieza de código, se documentaron algunas funciones.

--- src/main_thread_adc_entry.c
+++ src/main_thread_adc_entry.c
@@ -1,70 +1,61 @@
 #include <main_thread_adc.h>
 #include <main_thread.h>
-/*
- * variables
- */
 
 #define MAX_COUNTS 0xFFFFFFFF + 1
-uint32_t dutyCycle = 0;
-uint32_t u16ADC_Data = 0;
-/*
- Almacena el numero de pulsos por revoulción
- */
+uint16_t dutyCycle = 0;
+uint16_t u16ADC_Data = 0;
+
 uint32_t time = 0;
 uint32_t RPM = 0;
 uint32_t frecuencia = 0;
 
-
-/*
- Almacena el valor incrementado por el timer cada 100 microsegundos.
- */
 uint32_t timer = 0;
 
 ULONG my_message[3] =
 { 0, 0, 0 };
+
 /* thread_adc entry function */
 void main_thread_adc_entry(void)
 {
-    /* TODO: add your own code here */
-
     /*
-     * abrir, configurar ADC
+     * Open, start ADC
      */
     g_adc0.p_api->open (g_adc0.p_ctrl, g_adc0.p_cfg);
     g_adc0.p_api->scanCfg (g_adc0.p_ctrl, g_adc0.p_channel_cfg);
     g_adc0.p_api->scanStart (g_adc0.p_ctrl);
 
     /*
-     * abrir, correr timer1
+     * Open, start system Timer
      */
-
     systemTimer.p_api->open (systemTimer.p_ctrl, systemTimer.p_cfg);
     systemTimer.p_api->start (systemTimer.p_ctrl);
 
+    /*
+     * Open, start PWM Timer
+     */
     g_timer_pwm.p_api->open (g_timer_pwm.p_ctrl, g_timer_pwm.p_cfg);
     g_timer_pwm.p_api->start (g_timer_pwm.p_ctrl);
 
+    /*
+     * Open, enable Input Capture
+     */
     g_input_capture.p_api->open (g_input_capture.p_ctrl, g_input_capture.p_cfg);
     g_input_capture.p_api->enable (g_input_capture.p_ctrl);
 
 
     while (1)
     {
-
         g_adc0.p_api->read (g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
 
-        /*
-         * establecer rango 0-100
-         */
-
-        dutyCycle = ((u16ADC_Data * 100) / 4095);
+        /* Set 0-100 range*/
+        dutyCycle = (u16ADC_Data * 100) / 4095;
 
         g_timer_pwm.p_api->dutyCycleSet (g_timer_pwm.p_ctrl, dutyCycle, TIMER_PWM_UNIT_PERCENT, 1);
 
-        /*almacenamiento*/
-
+        /*Queue storage*/
         my_message[0] = dutyCycle;
-        /*enviar msj a thread 1*/
+
+        /*Send message to Display thread.*/
         tx_queue_send (&g_main_queue_display, my_message, TX_NO_WAIT);
         tx_thread_sleep (10);
     }
@@ -79,5 +70,6 @@ void input_capture_callback(input_capture_callback_args_t *p_args)
 
 void systemTimer_callback(timer_callback_args_t *p_args)
 {
+    (void)p_args;
     timer++;
 }
