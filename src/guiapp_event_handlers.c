32d822ccfd4b3b866436cadbcfb97b177e9a7f7d
Francisco <fcojair.dico@gmail.com>
2019-10-05 09:27:54 -0500

    Se agregó funcionalidad para desplegar el set point y la vel del motor en el display.

--- src/guiapp_event_handlers.c
+++ src/guiapp_event_handlers.c
@@ -3,7 +3,6 @@
 
 #include "main_thread.h"
 
-uint32_t t = 100;
 ULONG my_rcv_msg[3] = {0,0,0};
 
 static void update_duty(GX_WIDGET *p_widget);
@@ -34,16 +33,45 @@ UINT window1_handler(GX_WINDOW *widget, GX_EVENT *event_ptr)
 
 static void update_duty(GX_WIDGET *p_widget)
 {
-    /*recibir mensaje*/
+    /*Receive message*/
 
-   tx_queue_receive(&g_main_queue_display,my_rcv_msg, 20);
+    tx_queue_receive(&g_main_queue_display,my_rcv_msg, 20);
     ssp_err_t err = 0;
     static GX_CHAR txt[4];
     GX_PROMPT *p_prompt = NULL;
+
+    /*
+     * Functionality for displaying the duty value.
+     */
     err = gx_widget_find(p_widget, ID_LBL_DUTY_VALUE, GX_SEARCH_DEPTH_INFINITE, (GX_WIDGET **)&p_prompt);
     if(TX_SUCCESS == err)
     {
-        gx_utility_ltoa((LONG)  my_rcv_msg[0], txt,4 );
+        gx_utility_ltoa((LONG)  my_rcv_msg[0], txt, 4);
+        err = gx_prompt_text_set(p_prompt, txt);
+        if (err)
+            while (1);
+    }
+
+    /*
+     * Functionality for displaying the setpoint value.
+     */
+    err = gx_widget_find(p_widget, ID_LBL_SETPOINT_VALUE, GX_SEARCH_DEPTH_INFINITE, (GX_WIDGET **)&p_prompt);
+    if(TX_SUCCESS == err)
+    {
+        gx_utility_ltoa((LONG) my_rcv_msg[1], txt, 4);
+        err = gx_prompt_text_set(p_prompt, txt);
+        if (err)
+            while (1);
+    }
+
+    /*
+     * Functionality for displaying the speed motor value
+     */
+
+    err = gx_widget_find(p_widget, ID_LBL_SPEED_VALUE, GX_SEARCH_DEPTH_INFINITE, (GX_WIDGET **)&p_prompt);
+    if(TX_SUCCESS == err)
+    {
+        gx_utility_ltoa((LONG) my_rcv_msg[2], txt, 4);
         err = gx_prompt_text_set(p_prompt, txt);
         if (err)
             while (1);
